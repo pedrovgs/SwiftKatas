a5687c2fdc19919e1f6f246a5af5df5e28fdf6ac
Pedro Vicente Gómez Sánchez <pedrovicente.gomez@gmail.com>
2018-12-15 10:50:32 +0100

    Initial Commit

---
+++ SwiftKatas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftKatas
+//
+//  Created by Pedro Vicente Gomez on 15/12/2018.
+//  Copyright © 2018 Pedro's Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
